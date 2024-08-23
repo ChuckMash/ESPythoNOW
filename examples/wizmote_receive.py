7bb14d27aafcc159dcd2f59d16c264c92acc1745
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2024-08-22 20:00:01 -0700

    Added examples wizmote send and receive

---
+++ examples/wizmote_receive.py
@@ -0,0 +1,47 @@
+from ESPythoNOW import *
+import struct
+import collections
+
+
+
+
+
+class wizmote_receive:
+
+  def __init__(self, interface, callback):
+    self.callback      = callback
+    self.button_lookup = {1:"ON", 2:"OFF", 3:"SLEEP", 16:"1", 17:"2", 18:"3", 19:"4", 8:"-", 9:"+"}
+    self.recent        = collections.deque(maxlen=10)
+    self.espnow        = ESPythoNow(interface=interface, mac="DD:DD:DD:DD:DD:DD", callback=self.espnow_callback, accept_all=True)
+    self.espnow.start()
+
+
+
+  def espnow_callback(self, from_mac, to_mac, msg):
+    type, sequence, d1, button, d2, battery, ccm = struct.unpack("<BIBBBB4s", msg)
+
+    # Discard duplicate messages
+    if from_mac.encode()+ccm in self.recent:
+      return
+
+    # Button code not found, should not happen
+    if button not in self.button_lookup.keys():
+      return
+
+    self.recent.append(from_mac.encode()+ccm)
+
+    if callable(self.callback):
+      self.callback(from_mac, self.button_lookup[button])
+
+
+
+
+
+if __name__ == "__main__":
+
+  def wizmote_callback(from_mac, button):
+    print(from_mac, button)
+
+  wm = wizmote_receive(interface="wlp1s0", callback=wizmote_callback)
+
+  input() # Run until enter is pressed
---
+++ examples/wizmote_send.py
@@ -0,0 +1,73 @@
+from ESPythoNOW import *
+import struct
+import time
+import random
+
+
+
+
+class wizmote_sender:
+
+  def __init__(self, interface):
+    self.button_lookup = {"ON":1, "OFF":2, "SLEEP":3, "1":16, "2":17, "3":18, "4":19, "-":8, "+":9}
+    self.espnow = ESPythoNow(interface=interface, mac="DD:DD:DD:DD:DD:DD")
+    self.espnow.start()
+    self.sequence = 1
+
+
+
+  def send_button(self, button):
+    msg = b""
+    msg += struct.pack('<B',  0x91 if button == "ON" else 0x81) # Type
+    msg += struct.pack('<I',  self.sequence)                    # Sequence
+    msg += struct.pack('<B',  32)                               # Data type
+    msg += struct.pack('<B',  self.button_lookup[button])       # Button
+    msg += struct.pack('<B',  1)                                # Data type
+    msg += struct.pack('<B',  100)                              # Battery level
+    msg += struct.pack('<4B', *random.randbytes(4))             # CCM not implimented, may not work with actual WiZ lights
+    self.espnow.send("FF:FF:FF:FF:FF:FF", msg)
+    self.sequence+=1
+
+
+
+
+
+if __name__ == "__main__":
+  wm = wizmote_sender(interface="wlp1s0")
+
+  while True:
+    print("Sending ON Button")
+    wm.send_button("ON")
+    time.sleep(3)
+
+    print("Sending OFF Button")
+    wm.send_button("OFF")
+    time.sleep(3)
+
+    print("Sending SLEEP Button")
+    wm.send_button("SLEEP")
+    time.sleep(3)
+
+    print("Sending 1 Button")
+    wm.send_button("1")
+    time.sleep(3)
+
+    print("Sending 2 Button")
+    wm.send_button("2")
+    time.sleep(3)
+
+    print("Sending 3 Button")
+    wm.send_button("3")
+    time.sleep(3)
+
+    print("Sending 4 Button")
+    wm.send_button("4")
+    time.sleep(3)
+
+    print("Sending - Button")
+    wm.send_button("-")
+    time.sleep(3)
+
+    print("Sending + Button")
+    wm.send_button("+")
+    time.sleep(3)
