ad96e49e45da3c04a9a0e94edfb5a5d4f701022d
ChuckMash <86080247+ChuckMash@users.noreply.github.com>
2026-02-26 12:59:19 -0800

    Update ESPythoNOW.py

--- ESPythoNOW.py
+++ ESPythoNOW.py
@@ -790,7 +790,10 @@ def main():
       for k, v in options.items()
       if v is not None and v != ""
     })
-    """
+
+
+
+    
     # overwrite args with local mqtt, needs work
     import urllib.request
     token = os.environ.get("SUPERVISOR_TOKEN", "")
@@ -805,8 +808,9 @@ def main():
       args.mqtt_username = data['username']
       args.mqtt_password = data['password']
  
-      print(args)
-      """
+
+  
+  
   else:
     args = parser.parse_args()
   
